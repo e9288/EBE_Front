c92dba150e5e6e961ff6d5d05d242cc22c18e19c
e9288 <e9288@naver.com>
2020-01-31 01:05:17 +0900

    SideBar error 잡아야함

--- src/component/MenuBar/MenuBar.js
+++ src/component/MenuBar/MenuBar.js
@@ -1,11 +1,22 @@
 import React, { Fragment } from 'react';
-import { Navbar } from 'react-bootstrap';
+import {Navbar, Nav, NavItem, Button, Glyphicon} from 'react-bootstrap';
+import { Sidebar } from 'react-bootstrap-sidebar';
 
+const SideBar = require('react-bootstrap-sidebar');
 class MenuBar extends React.Component {
     render() {
         return (
             <Fragment>
-                <Navbar expand="lg" variant="light" bg="light">
+                <Sidebar side='left' isVisible={ this.state.isVisible } onHide={ () => this.updateModal(false) }>
+                    <Nav>
+                      <NavItem href="#">Link 1</NavItem>
+                      <NavItem href="#">Link 2</NavItem>
+                      <NavItem href="#">Link 3</NavItem>
+                      <NavItem href="#">Link 4</NavItem>
+                    </Nav>
+                </Sidebar>
+                
+                <Navbar expand="sm" variant="light" bg="light">
                     <Navbar.Brand href="#">Navbar</Navbar.Brand>
                 </Navbar>
             </Fragment>
