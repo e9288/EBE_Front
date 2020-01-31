2a0f8af68dd578f7401a702455b5eb3f705ff5ef
e9288 <e9288@naver.com>
2020-02-01 03:06:48 +0900

    chat Component UserChatPane&EmilyChatPane 구현(60%)

--- src/component/Content/Chat.js
+++ src/component/Content/Chat.js
@@ -1,30 +1,93 @@
 import React, { Fragment } from 'react';
-import { InputGroup, FormControl, Button } from 'react-bootstrap';
+import { InputGroup, FormControl, Button, Card } from 'react-bootstrap';
+
+
 class Chat extends React.Component {
+    
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            userMsg: '',
+            emilyMsg: ''
+        }
+        this.putUserMsg = this.putUserMsg.bind(this);
+    }
+
+    userMsg = '';
+
+    async putUserMsg(msg) {
+        if(this.state.userMsg !== msg){
+            this.setState(
+                {userMsg: msg}
+            );
+            this.setState(
+                {emilyMsg: await this.putEmilyMsg(msg)}
+            );
+        }
+    }
+
+    async putEmilyMsg(msg) {
+        return 'Emily Msg';
+    }
+
     render() {
         return (
             <div>
-                <TextArea />
-                <TextInput />
+                <UserSpeakPan
+                    text={this.state.userMsg}
+                />
+                <EmilySpeakPan 
+                    text={this.state.emilyMsg}
+                />
+                <TextInput 
+                    putUserMsg={this.putUserMsg}
+                />
             </div>
         );
     }
 }
 
 
-class TextArea extends React.Component {
-    render() {
-        return (
-            <Fragment>
-                <InputGroup>
-                    <FormControl as="textarea" aria-label="With textarea" size='lg' readOnly='true' />
-                </InputGroup>
-            </Fragment>
-        );
-    }
-}
+// class TextArea extends React.Component {
+//     render() {
+//         return (
+//             <Fragment>
+//                 <InputGroup>
+//                     <FormControl className="chatArea" as="textarea" aria-label="With textarea" size='lg' readOnly='true' style={{ height:  }} />
+//                 </InputGroup>
+//             </Fragment>
+//         );
+//     }
+// }
 
 class TextInput extends React.Component {
+
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            value: ''
+        };
+        
+        this.handleKeyPress = this.handleKeyPress.bind(this);
+    }
+
+    componentDidUpdate() {
+        this.putUserMsg();
+    }
+
+    putUserMsg = () => {
+        this.props.putUserMsg(this.state.value);
+    }
+
+    handleKeyPress = (e) => {
+        if(e.charCode === 13) {
+            this.setState({value: e.target.value});
+            e.target.value = '';
+        }
+    }
+
     render() {
         return (
             <Fragment>
@@ -33,6 +96,7 @@ class TextInput extends React.Component {
                         placeholder="entry any value..."
                         aria-label="Recipient's username"
                         aria-describedby="basic-addon2"
+                        onKeyPress={this.handleKeyPress}
                     />
                     <InputGroup.Append>
                         <Button variant="outline-secondary">Enter</Button>
@@ -40,6 +104,57 @@ class TextInput extends React.Component {
                 </InputGroup>
             </Fragment>
         );
+    }
+}
+
+class UserSpeakPan extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.text = props.text;
+        this.valGbnCd = props.valGbnCd;
+    }
+    text = '';
+    valGbnCd = '';
+    render() {
+        return (
+            <Fragment>
+                <Card border="primary" style={{ width: '18rem', right: 0, }}>
+                    <Card.Header>User</Card.Header>
+                    <Card.Body>
+                        <Card.Text>
+                            {this.props.text}
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </Fragment>
+        )
+    }
+}
+
+class EmilySpeakPan extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.text = props.text;
+        this.valGbnCd = props.valGbnCd;
+    }
+    text = '';
+    valGbnCd = '';
+    render() {
+        return (
+            <Fragment>
+                <Card border="info" style={{ width: '18rem', left: 0 }}>
+                    <Card.Header>Emily</Card.Header>
+                    <Card.Body>
+                        
+                        <Card.Text>
+                            {this.props.text}
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </Fragment>
+        )
     }
 }
 export default Chat;
