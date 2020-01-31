754d863482c57598c25923d9b611a66a09ed7042
dhkim <dhkim@iandna.com>
2020-01-31 15:12:24 +0900

    bootstrap navbar

--- src/component/MenuBar/MenuBar.js
+++ src/component/MenuBar/MenuBar.js
@@ -1,23 +1,31 @@
 import React, { Fragment } from 'react';
-import {Navbar, Nav, NavItem, Button, Glyphicon} from 'react-bootstrap';
-import { Sidebar } from 'react-bootstrap-sidebar';
+import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
 
-const SideBar = require('react-bootstrap-sidebar');
+
 class MenuBar extends React.Component {
     render() {
         return (
             <Fragment>
-                <Sidebar side='left' isVisible={ this.state.isVisible } onHide={ () => this.updateModal(false) }>
-                    <Nav>
-                      <NavItem href="#">Link 1</NavItem>
-                      <NavItem href="#">Link 2</NavItem>
-                      <NavItem href="#">Link 3</NavItem>
-                      <NavItem href="#">Link 4</NavItem>
-                    </Nav>
-                </Sidebar>
-                
-                <Navbar expand="sm" variant="light" bg="light">
-                    <Navbar.Brand href="#">Navbar</Navbar.Brand>
+                <Navbar bg="light" expand="lg">
+                    <Navbar.Brand href="#home">Echo By Emily</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="mr-auto">
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#link">Link</Nav.Link>
+                            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                        <Form inline>
+                            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                            <Button variant="outline-success">Search</Button>
+                        </Form>
+                    </Navbar.Collapse>
                 </Navbar>
             </Fragment>
         );
