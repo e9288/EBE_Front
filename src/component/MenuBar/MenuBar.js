71f64bb03feb4f2ba810687557c8ca4562a9da91
dhkim <dhkim@iandna.com>
2020-01-31 15:19:14 +0900

    nav menu mod

--- src/component/MenuBar/MenuBar.js
+++ src/component/MenuBar/MenuBar.js
@@ -11,8 +11,9 @@ class MenuBar extends React.Component {
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="mr-auto">
-                            <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#link">Link</Nav.Link>
+                            <Nav.Link href="#home">Intro</Nav.Link>
+                            <Nav.Link href="#link">Chat</Nav.Link>
+                            {/*                             
                             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
@@ -20,6 +21,7 @@ class MenuBar extends React.Component {
                                 <NavDropdown.Divider />
                                 <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
                             </NavDropdown>
+                             */}
                         </Nav>
                         <Form inline>
                             <FormControl type="text" placeholder="Search" className="mr-sm-2" />
